c58049bb7e297a863d5d3a77e43033eb8af3eba8
Sam <zhao.shengjun@gmail.com>
2018-07-07 17:47:31 +1000

    friendly chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'random_words_state.dart';
+import 'chat_screen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -7,9 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      home: new RandomWords(),
-      theme: new ThemeData(primaryColor: Colors.white),
+      title: 'FriendlyChat',
+      home: new ChatScreen(),
     );
   }
 }
