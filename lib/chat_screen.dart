faba35e998065e0e0ac0f53176fc7ea2c29e1238
Sam <zhao.shengjun@gmail.com>
2018-07-07 19:06:21 +1000

    display message

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'chat-message.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -6,15 +7,30 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('FriendlyChat'),
-      ),
-      body: _buildTextComposer(),
-    );
+        appBar: new AppBar(title: new Text('FriendlyChat')),
+        body: new Column(
+          children: <Widget>[
+            new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
+            ),
+            new Divider(height: 1.0),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            )
+          ],
+        ));
   }
 
   Widget _buildTextComposer() {
@@ -26,8 +42,7 @@ class ChatScreenState extends State<ChatScreen> {
               child: new TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
-                decoration:
-                    new InputDecoration.collapsed(hintText: "Send a message"),
+                decoration: new InputDecoration.collapsed(hintText: "Send a message"),
               ),
             ),
             new Container(
@@ -43,5 +58,13 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 }
